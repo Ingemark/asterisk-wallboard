1723701d441372f57b368dd0d81c9a081bd0b55e
Viktor Matic <viktor@inge-mark.hr>
2013-03-20 13:48:35 +0100

    Initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EscalationSystem::Application.config.secret_token = '4f8640af86c50ec56a4c76f3f1044b3a22af5114db49342dc86f7e2f88461b3dd22b343e0002f4d7782a4a0a2273551b669f76593722590e8854f95a9f9c48f8'
