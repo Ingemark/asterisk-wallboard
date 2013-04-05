b88a1fc85673a6a1abf5b122bccd5e617bc7369c
Bruno <bruno@bruno-DELL.(none)>
2013-04-05 20:20:21 +0200

    Added action buttons and alerts. Added ruby interface for pbxis-ws.

--- lib/pbxis.rb
+++ lib/pbxis.rb
@@ -1,28 +1,49 @@
-module PbxisFunctions
-  # Method logs user on/off.
-  #
-  # * *Args*  :
-  #   - +user+  ->  user to log in
-  #   - +queue_id+  ->  id of the queue
-  #   - +action+  -> add or remove
-  def self.log_on_off(user, queue_id, action)
-    if action.to_s != "add" and action.to_s != "remove"
-      raise "Unsuported action"
+# Module provides API for pbxis-ws.
+module Pbxis
+  
+  class PbxisWS
+    
+    HOST_ADDR = "http://#{Settings.pbxisws[:host]}:#{Settings.pbxisws[:port]}"
+
+    def self.get_ticket(queues, agents)
+      params = {
+        :queues => queues,
+        :agents => agents
+      }
+      RestClient.post("#{HOST_ADDR}/ticket", params.to_json, :content_type => :json).to_str.gsub('"', '')
+    end
+        
+    def self.log_on(agent, queue)
+      params = {
+        :queue => queue,
+        :agent => agent
+      }
+      begin
+          ActiveSupport::JSON.decode(RestClient.post("#{HOST_ADDR}/queue/add", params.to_json, :content_type => :json))
+      rescue => e
+        raise "An error occurred while trying to log agent on: #{e.message}"
+      end
     end
     
-    if user.pbx_queues.exists?(queue_id)
-      pbxis_params = {
-        :queue => PbxQueue.find(queue_id).name,
-        :interface => user.extension
+    def self.log_off(agent, queue)
+      params = {
+        :queue => queue,
+        :agent => agent
       }
-      
       begin
-          ActiveSupport::JSON.decode(RestClient.post("http://#{Settings.pbxisws[:host]}:#{Settings.pbxisws[:port]}/queue/#{action.to_s}", pbxis_params))
+          ActiveSupport::JSON.decode(RestClient.post("#{HOST_ADDR}/queue/remove", params.to_json, :content_type => :json))
       rescue => e
-        raise "An error occurred while trying to log agent #{action.to_s == "add"?"on":"off"}: #{e.message}"
+        raise "An error occurred while trying to log agent on: #{e.message}"
       end
-    else
-      raise "Queue with id \'#{params[:queue_id]}\' doesn't exist."
+    end
+    
+    def self.get_status queue
+      status = ActiveSupport::JSON.decode(RestClient.get("#{HOST_ADDR}/queue/status?queue=#{queue.to_s}"))[0]
+      
+      # transform result
+      status["members"] = Hash[status["members"].map { |m| [m["memberName"].gsub("SIP/", ""), m] }]
+      status["members"].each_value { |m| m["memberName"] = m["memberName"].gsub("SIP/", "") }
+      status
     end
   end
 end
