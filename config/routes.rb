12bf53f6315a333db9fb53c5b935e641a7b5620f
Bruno <bruno@bruno-DELL.(none)>
2013-04-07 16:33:52 +0200

    Adder new route for manager - queue stats reset.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ WallboardSystem::Application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
-  match "/agent" => "agent#index"
+  # Manager routes
   match "/manager" => "manager#index"
   match "/manager/agent-logoff/:agent_id/:queue" => "manager#agent_logoff", :as => "manager_agent_logoff"
+  match "/manager/reset-queue-stats/:queue" => "manager#reset_queue_stats", :as => "manager_reset_queue_stats"
+  
+  # Agent routes
+  match "/agent" => "agent#index"
   match "/agent/logon/:queue_id" => "agent#logon", :as => "agent_logon"
   match "/agent/logoff/:queue_id" => "agent#logoff", :as => "agent_logoff"
 end
